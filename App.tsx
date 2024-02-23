11fcbf0df1fb04490358de1e1857c97fa9ac9229
Mael <goujonmael@gmail.com>
2024-02-23 17:09:07 +0100

    tuto doc React

--- App.tsx
+++ App.tsx
@@ -1,118 +1,213 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useRef, useState } from 'react';
+import { Alert, Button, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+type CatProps = {
+  name: string;
+};
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Coucou = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>Coucou</Text>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Cat = (props: CatProps) => {
+  const [isHungry, setIsHungry] = useState(true);
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ], {
+      cancelable: true,
+      onDismiss: () =>
+        Alert.alert(
+          'This alert was dismissed by tapping outside of the alert dialog.', 'okok', [
+          {
+            text: 'OK',
+            onPress: () => console.log('OK Pressed'),
+            style: 'cancel',
+          },
+        ], {
+          cancelable: true,
+        }
+        ),
+    },
+    );
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.container}>
+      <Text>
+        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!
+      </Text>
+      <TouchableOpacity onPress={() => {
+        setIsHungry(!isHungry);
+      }}
+        style={styles.loginScreenButton}>
+        <Text style={isHungry ? styles.loginText : styles.loginTextSelected}>{isHungry ? 'Pour me some milk, please!' : 'Thank you!'}</Text>
+      </TouchableOpacity>
+      <Button onPress={() => Alert.alert('Button with adjusted color pressed')}
+        title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
+        color={isHungry ? 'red' : 'green'}
+        accessibilityLabel="Learn more about this purple button"
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Button title={'2-Button Alert'} onPress={createTwoButtonAlert} />
+      <Button title={'3-Button Alert'} onPress={createThreeButtonAlert} />
+    </View>
   );
-}
+};
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{ padding: 10 }}>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{ padding: 10, fontSize: 42 }}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
+    </View>
+  );
+};
+
+const FlexDimensionsBasics = () => {
+  return (
+    // Try removing the `flex: 1` on the parent View.
+    // The parent will not have dimensions, so the children can't expand.
+    // What if you add `height: 300` instead of `flex: 1`?
+    <View style={{ height: 300 }}>
+      <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
+      <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+      <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+    </View>
+
+  );
+};
+
+const ImageExample = ({ scrollViewRef }: { scrollViewRef: React.RefObject<ScrollView> }) => (
+  <View >
+    <View style={styles.containerImages}>
+      <Image
+        source={{ uri: 'https://reactnative.dev/docs/assets/p_cat1.png' }}
+        style={styles.image}
+      />
+      <Image
+        source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }}
+        style={[styles.image, { marginLeft: 20 }]} // add a margin to create some space between the images
+      />
+    </View>
+    
+    <TouchableOpacity onPress={() => scrollViewRef.current?.scrollTo({ x: 0, y: 0, animated: true })}>
+      <View style={styles.thirdImageContainer}>
+        <Image source={{ uri: 'https://reactjs.org/logo-og.png' }}
+          style={styles.largeImage}
+        />
+      </View>
+    </TouchableOpacity>
+  </View>
+);
+
+const Cafe = () => {
+  const scrollViewRef = useRef<ScrollView>(null); // Create a ref for the ScrollView
+
+  return (
+
+
+    <ScrollView ref={scrollViewRef}>
+      <Cat name="Munkustrap" />
+      <Cat name="Spot" />
+      <PizzaTranslator />
+      <FlexDimensionsBasics />
+      <ImageExample scrollViewRef={scrollViewRef} />
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  loginScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#1E6738',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  loginText: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  loginTextSelected: {
+    color: 'red',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    paddingVertical: 10,
+    marginTop: 20,
   },
+  redBackground: {
+    backgroundColor: 'red',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  containerImages: {
+    flexDirection: 'row', // set the direction to row for placing images side by side
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  thirdImageContainer: {
+    marginTop: 20, // add some space between the first two images and the third one
+    backgroundColor: 'yellow', // add a background color to the container
+    alignItems: 'center', // align the items to the center
+
+  },
+  largeImage: {
+    minHeight: 200, // set a minimum height for the image
+    minWidth: 200, // set a minimum width for the image
+    aspectRatio: 1, // set the aspect ratio to 1:1
+    margin: 100, // add some margin to the image
+    flex: 1, // set the flex property to 1
+    borderRadius: 50, // set the border radius to 100
+
+
+  }
 });
 
-export default App;
+
+export default Cafe;
